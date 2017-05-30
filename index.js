cc1d161a89f9823514c04b26260ffdfa08a2dec9
Cozy <contact@cozycloud.cc>
2017-05-30 16:47:36 +0000

    publish: fix: bad carriage return
    generated from commit 8c4b37b45a660b5ac93af119269d6ccd18b06956

--- index.js
+++ index.js
@@ -73478,8 +73478,7 @@ function parsePage (requiredFields, bills, data, next) {
 
     // Get the facture id and build the download url from it.
     const id = $(this).attr('facture-id')
-    const params =
-    {id}
+    const params = {id}
     const url = `${baseDlUrl}?${qs.stringify(params)}`
 
     // Build bill object.
